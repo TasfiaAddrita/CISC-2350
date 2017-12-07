6be61952f772311a86c86d7ae38ed1a1ec6ec2fb
Tasfia Addrita <TasfiaAddrita@users.noreply.github.com>
2017-12-07 15:18:55 -0500

    Add files via upload

--- Final/website/js/main.js
+++ Final/website/js/main.js
@@ -1,44 +1,52 @@
 $(document).ready(function() {
+
+    $(document).mousemove(function(event) {
+        $(".circle").css("left", event.clientX);
+        $(".circle").css("top", event.clientY);
+    })
+
     $("nav li a:lt(2)").click(function() {
-        $("#landing").css("width", "35%");
+        if ($(window).width() > 480) {
+           $("#landing").css("width", "35%");
+        }
+        else {
+           $("#landing").css("width", "100%");
+        }
         $("nav li a").css("color", "black");
-        // $(this).css("text-decoration", "underline");
         $(".right-panel").css("display", "flex");
-        // $(this).css("display", "inline");
     })
 
     $("#landing h1").click(function() {
         $("#landing").css("width", "100%");
         $("nav li a").css("color", "white");
         $(".right-panel").css("display", "none");
+        $(".circle").css("display", "block");
     })
 
     $("#nav-about").click(function() {
         $("#about").css("display", "flex");
         $("#projects").css("display", "none");
-        // $("#contact").css("display", "none");
+        $(".circle").css("display", "none");
     })
 
     $("#nav-projects").click(function() {
         $("#projects").css("display", "flex");
         $("#about").css("display", "none");
-        // $("#contact").css("display", "none");
     })
 
     $("#nav-contact").click(function() {
         $("#contact").toggle();
-        // $("#contact").css("display", "block");
-        // $("#about").css("display", "none");
-        // $("#projects").css("display", "none");
     })
 
-    // $("#nav-contact").toggle();
+    $("#experience-header").click(function() {
+        $("#experience-box").toggle();
+    })
 
-    // $("#about").click(function() {
-    //
-    // })
+    $("#leadership-header").click(function() {
+        $("#leadership-box").toggle();
+    })
 
-    // $('a[href$="#about"]').click(function() {
-    //     $("#about").css("display", "flex");
-    //})
+    $("#skills-header").click(function() {
+        $("#skills-box").toggle();
+    })
 });
